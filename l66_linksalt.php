26cbf7812395c1085a9e832599ebbc9f8afc4c03
owagu <owagu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-27 01:33:00 +0000

    git-svn-id: https://plugins.svn.wordpress.org/lane66-affiliate-tools/trunk@365274 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ l66_linksalt.php
@@ -0,0 +1,5 @@
+<iframe src="http://linksalt.com" width="100%" height="1400" scrolling="no"></iframe>
+<div style="text-align:center;font-size:11px;color:#ccc">
+linksalt is a stand-alone product from linksalt.com. It is provided in this plugin as a courtesy feature.<br>
+There are no related tracking, monitoring, data-sharing, or member features links between linksalt and this plugin.
+</div>
